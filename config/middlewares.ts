d81051467936e920e820e74f55044b67c84945d8
Joel Chubrega <jchubrega@gmail.com>
2026-02-19 20:16:36 -0300

    fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,13 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://aleconstrucciones-frontend.vercel.app'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
