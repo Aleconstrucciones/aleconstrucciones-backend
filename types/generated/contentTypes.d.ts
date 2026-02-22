423ad3a8d6d963ef62533ccfb61cde168f0135ac
Joel Chubrega <jchubrega@gmail.com>
2026-02-22 03:25:05 -0300

    update form

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -516,30 +516,9 @@ export interface ApiContactRequestContactRequest
     message: Schema.Attribute.Text;
     name: Schema.Attribute.String & Schema.Attribute.Required;
     phone: Schema.Attribute.String & Schema.Attribute.Required;
-    projectType: Schema.Attribute.Enumeration<
-      [
-        'Alba\u00F1iler\u00EDa',
-        'Carpinter\u00EDa',
-        'Cerramientos',
-        'Climatizaci\u00F3n',
-        'Construcciones Generales',
-        'Electricidad',
-        'Gasista',
-        'Herrer\u00EDa',
-        'Impermeabilizaciones',
-        'Instalaci\u00F3n de pisos',
-        'Pintura',
-        'Piscinas',
-        'Plomer\u00EDa',
-        'Servicios de dise\u00F1o y direcci\u00F3n',
-        'Techos',
-        'Trabajos en altura',
-        'Vidrier\u00EDa',
-        'Yeser\u00EDa',
-      ]
-    >;
+    projectType: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
-    type: Schema.Attribute.Enumeration<['Contacto', 'Cotizacion']>;
+    type: Schema.Attribute.Enumeration<['Consulta', 'Cotizacion']>;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
