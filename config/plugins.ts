53bd6e72374b8460538fd95edb7a21e9709e344c
Joel Chubrega <jchubrega@gmail.com>
2026-02-25 12:05:55 -0300

    fix plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,7 +6,9 @@ export default ({ env }) => ({
                 cloud_name: env("CLOUDINARY_CLOUD_NAME"),
                 api_key: env("CLOUDINARY_API_KEY"),
                 api_secret: env("CLOUDINARY_API_SECRET"),
-                folder: env("CLOUDINARY_FOLDER", "production"),
+                uploadOptions: {
+                    folder: env("CLOUDINARY_FOLDER", "production"),
+                },
             },
         },
     },
